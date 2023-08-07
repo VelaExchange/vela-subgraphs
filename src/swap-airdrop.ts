37e5744075eb29727e07f723a67e41057e593dff
Love Trading <philipacho28@gmail.com>
2023-08-07 18:46:58 -0500

    integrate AirdropForHyperVLP contract and getUserAirdropVLPStats and publish v0.0.43

--- src/swap-airdrop.ts
+++ src/swap-airdrop.ts
@@ -1,11 +1,16 @@
   import { BigInt } from "@graphprotocol/graph-ts"
-import {
+  import {
     Claim as ClaimEvent,
     Swap as SwapEvent
     } from "../generated/SwapAndAirdrop/SwapAndAirdrop"
-  
+
+  import {
+    AirdropDistributed
+    } from "../generated/AirdropForHyperVLP/AirdropForHyperVLP"
+      
   import {
       UserAirdropStat,
+      UserVLPRewardStat
     } from "../generated/schema"
 
     export function handleClaim(event: ClaimEvent): void {
@@ -23,4 +28,12 @@ import {
 
     export function handleSwap(event: SwapEvent): void {
 
+    }
+
+    export function handleAirdropDistributed(event: AirdropDistributed): void {
+      let userVLPRewardStat = new UserVLPRewardStat(event.params.account.toHexString() + "-" + event.block.timestamp.toString())
+      userVLPRewardStat.account = event.params.account.toHexString()
+      userVLPRewardStat.amount = event.params.amount
+      userVLPRewardStat.timestamp = event.block.timestamp.toI32()
+      userVLPRewardStat.save()
     }
